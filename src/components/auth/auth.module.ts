e8d083f2612726481088e69744f3b55a80ba5135
Ng <erondung@gmail.com>
2021-11-01 20:53:42 +0100

    - Added swagger for api documentation - Added implementations for authentication and authorization with JWT - Added implementations to retrieve configurations from .env

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JWTStrategy } from '../strategy/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
